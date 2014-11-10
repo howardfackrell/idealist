ea88d8d4ed635c237a303b2dd144b31a7f0d5e00
Howard Fackrell <howard.fackrell@octanner.com>
2014-11-09 21:43:10 -0700

    changed color scheme, add javascript functionality to remove, add, complete list items

---
+++ javascript/idealist.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $(".shopping-list").on("click", ".fa-remove", function() {
+  	$(this).closest('li').remove();
+  });
+
+  $(".entryfield").keypress(function(event) {
+    if (event.which == 13) {
+    	event.preventDefault();
+    	var entryfield = $(".entryfield");
+    	var newItem = entryfield.val();
+    	entryfield.val('');
+
+        console.log("new item is: " + newItem);
+    	var t = $("<li><input type='checkbox' /><p>" + newItem + "</p><a class='fa fa-remove'></a></li>");
+    	$(".shopping-list > ul").prepend(t);
+    }
+  });
+
+  $(".shopping-list").on('click', "input[type=checkbox]", function() {
+  	console.log("click happens");
+    if ($(this).is(':checked')) {
+      $(this).closest("li").addClass("done");
+    } else {
+      $(this).closest("li").removeClass("done");	
+    }
+  });
+});
